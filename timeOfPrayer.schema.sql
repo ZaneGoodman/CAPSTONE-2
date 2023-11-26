37d17913257471f6ed2743ff86a931c5a24cce8a
Zane_Goodman <zgoodman1776@gmail.com>
2023-11-26 10:24:27 -0500

    completed user-flow & schema plan diagrams

---
+++ timeOfPrayer.schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    username VARCHAR(20) PRIMARY KEY CHECK (username = lower(username)),
+    password TEXT NOT NULL,
+
+)
+
+CREATE TABLE prayer_tracker (
+    username VARCHAR(25)
+        REFERENCES users ON DELETE CASCADE,
+    notes TEXT,
+    has_prayed BOOLEAN NOT NULL DEFAULT FALSE 
+)
